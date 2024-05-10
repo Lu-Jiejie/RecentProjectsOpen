b4531795f80ee2e7ac638a2670a8297d121f2d5c
Attack825 <2707138687@qq.com>
2024-05-10 11:12:23 +0800

    modified:   .gitignore 	modified:   main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import sys,os
+import webbrowser
+from flowlauncher import FlowLauncher
+import sys
+import os
 parent_folder_path = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(parent_folder_path)
 sys.path.append(os.path.join(parent_folder_path, 'lib'))
 sys.path.append(os.path.join(parent_folder_path, 'plugin'))
-
-from flowlauncher import FlowLauncher
-import webbrowser
 
 
 class HelloWorld(FlowLauncher):
@@ -13,7 +13,7 @@ class HelloWorld(FlowLauncher):
     def query(self, query):
         return [
             {
-                "title": "Hello World, this is where title goes. {}".format(('Your query is: ' + query , query)[query == '']),
+                "title": "Hello World, this is where title goes. {}".format(('Your query is: ' + query, query)[query == '']),
                 "subTitle": "This is where your subtitle goes, press enter to open Flow's url",
                 "icoPath": "Images/app.png",
                 "jsonRPCAction": {
@@ -29,17 +29,18 @@ class HelloWorld(FlowLauncher):
             {
                 "title": "Hello World Python's Context menu",
                 "subTitle": "Press enter to open Flow the plugin's repo in GitHub",
-                "icoPath": "Images/app.png", # related path to the image
+                "icoPath": "Images/app.png",  # related path to the image
                 "jsonRPCAction": {
                     "method": "open_url",
                     "parameters": ["https://github.com/Flow-Launcher/Flow.Launcher.Plugin.HelloWorldPython"]
                 },
-                "score" : 0
+                "score": 0
             }
         ]
 
     def open_url(self, url):
         webbrowser.open(url)
+
 
 if __name__ == "__main__":
     HelloWorld()
