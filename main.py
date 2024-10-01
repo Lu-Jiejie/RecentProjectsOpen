f2d37db7564b122dc1b5432c42356e6c44ffb66a
lvris <lvris@qq.com>
2024-10-01 22:24:33 +0300

    🐛 Fix import issue

--- main.py
+++ main.py
@@ -3,14 +3,13 @@
 import os
 import sys
 
-from plugin.main import RecentProjectsOpen
-
 parent_folder_path = os.path.abspath(os.path.dirname(__file__))
 # add the plugin folder to the sys.path
 sys.path.append(parent_folder_path)
 sys.path.append(os.path.join(parent_folder_path, "lib"))
 sys.path.append(os.path.join(parent_folder_path, "plugin"))
 
+from plugin.main import RecentProjectsOpen
 
 if __name__ == "__main__":
     RecentProjectsOpen()
